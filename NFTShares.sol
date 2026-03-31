de062d7f1263aca7722bb2aec26a973255d30ce0
Kobita <161524322+kobita1122@users.noreply.github.com>
2026-03-31 14:59:41 +0600

    Create NFTShares.sol

---
+++ NFTShares.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTShares is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, address _vault) 
+        ERC20(name, symbol) 
+        Ownable(_vault) 
+    {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
